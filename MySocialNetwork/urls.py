202e4a6b0c4d77a460d70e0867805b195fcf407b
Maksymilianx <79858139+Maksymilianx@users.noreply.github.com>
2021-10-02 17:19:27 +0200

    fix urls

--- MySocialNetwork/urls.py
+++ MySocialNetwork/urls.py
@@ -18,9 +18,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from account import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
+    path('', views.dashboard, name='dashboard'),
 ]
 
 if settings.DEBUG:
