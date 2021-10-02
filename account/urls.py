dc6d78aad36aa11341418dd5765c40aed0e39bc7
Maksymilianx <79858139+Maksymilianx@users.noreply.github.com>
2021-10-02 17:19:18 +0200

    fix urls

--- account/urls.py
+++ account/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     # path('login/', views.UserLogin.as_view(), name='login'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('', views.dashboard, name='dashboard'),
     # path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
     # path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
     # path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
@@ -15,6 +14,6 @@ urlpatterns = [
     # path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     # path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('', include('django.contrib.auth.urls')),
-    path('register/', views.Register.as_view(), name='register'),
-    path('edit/', views.Edit.as_view(), name='edit'),
+    path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
 ]
