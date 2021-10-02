fcaf47f432c809bbd638a6c882c980e62d9b711e
Maksymilianx <79858139+Maksymilianx@users.noreply.github.com>
2021-10-02 13:17:01 +0200

    update forms

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,23 @@
 from django import forms
+from django.contrib.auth.models import User
 
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Hasło', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Powtórz hasło', widget=forms.PasswordInput)
+
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'email')
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Hasła nie są identyczne.')
+        return cd['password2']
