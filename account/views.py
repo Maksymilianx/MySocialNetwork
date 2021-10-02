7a4b10efe5043d4aa0f4977e0cac7b69535339c0
Maksymilianx <79858139+Maksymilianx@users.noreply.github.com>
2021-10-02 17:19:08 +0200

    update views

--- account/views.py
+++ account/views.py
@@ -10,7 +10,7 @@ from account.models import Profile
 
 class UserLogin(View):
 
-    def get(self,request):
+    def get(self, request):
         form = LoginForm()
         return render(request, 'account/login.html', {'form': form})
 
@@ -35,13 +35,8 @@ def dashboard(request):
     return render(request, "account/dashboard.html", {'section': 'dashboard'})
 
 
-class Register(View):
-
-    def get(self, request):
-        user_form = UserRegistrationForm()
-        return render(request, "account/register.html", {'user_form': user_form})
-
-    def post(self, request):
+def register(request):
+    if request.method == 'POST':
         user_form = UserRegistrationForm(request.POST)
         if user_form.is_valid():
             new_user = user_form.save(commit=False)
@@ -49,27 +44,20 @@ class Register(View):
             new_user.save()
             profile = Profile.objects.create(user=new_user)
             return render(request, "account/register_done.html", {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'account/register.html', {'user_form': user_form})
 
 
-class Edit(View):
-
-    @login_required
-    def get(self, request):
-        user_form = UserEditForm(instance=request.user)
-        profile_form = ProfileEditForm(instance=request.user.profile)
-        return render(request, "account/edit.html", {'user_form': user_form, 'profile_form': profile_form})
-
-    @login_required
-    def post(self, request):
+@login_required
+def edit(request):
+    if request.method == 'POST':
         user_form = UserEditForm(instance=request.user, data=request.POST)
         profile_form = ProfileEditForm(instance=request.user.profile, data=request.POST, files=request.FILES)
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
-        return render(request, "account/edit.html", {'user_form': user_form, 'profile_form': profile_form})
-
-
-
-
-
-
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+    return render(request, "account/edit.html", {'user_form': user_form, 'profile_form': profile_form})
